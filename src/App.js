61f66410662a4d85c3c8440d5f25539faddacd19
Nyein Mg <nyeinchanaung.myeik@gmail.com>
2022-04-28 09:08:09 +0630

    State reference and testing output

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      userName: 'Name1',
+    };
+  }
+
   render = () => (
     <div className='container'>
       <div className='row'>
         <div className='col-12'>
           <h2 className='bg-primary text-white text-center p2'>
-            To do list
+            {this.state.userName} To do list
           </h2>
         </div>
       </div>
