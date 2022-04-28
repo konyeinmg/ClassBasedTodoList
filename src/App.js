4078c5a2c742de5506ac4c66ac7a7baf66d27d8a
Nyein Mg <nyeinchanaung.myeik@gmail.com>
2022-04-28 09:20:20 +0630

    Add and Display todo items

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-      userName: 'Name1',
+      userName: 'Edward',
+      todoItems: [
+        {action: 'Buy Milk', done: false},
+        {action: 'Dentist at 5pm', done: false},
+        {action: 'Go to Gym', done: false},
+      ],
     };
   }
 
-  changeStateData = () => {
-    this.setState({
-      userName: this.state.userName === 'Name1' ? 'Name2' : 'Name1',
-    });
-  }
+  todoRows = () => 
+    this.state.todoItems.map((item) => (
+      <tr key={item.action}>
+        <td>{item.action}</td>
+      </tr>
+    ));
+  
 
   render = () => (
     <div className='container'>
@@ -22,9 +29,18 @@ export default class App extends Component {
           <h2 className='bg-primary text-white text-center p2'>
             {this.state.userName} To do list
           </h2>
-          <button className='btn btn-secondary m-2' onClick={this.changeStateData}>
-            Change
-          </button>
+        </div>
+        <div className='col-12'>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>Task</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.todoRows()}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
